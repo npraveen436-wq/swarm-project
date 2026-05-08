d9d70fbe3071c80e35fc0cd40d8787b21e617fae
P Nethu <n.praveen436@gmail.com>
2026-05-08 10:05:48 +0000

    Update heading to v2

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send(`
     <html>
       <body style="font-family: sans-serif; text-align: center; padding: 50px;">
-        <h1>🐳 Hello from Docker Swarm!</h1>
+        <h1>🐳 Hello from Docker Swarm!v2</h1>
         <p>Served by container on host: <b>${os.hostname()}</b></p>
         <p>Refresh the page — you'll see different hostnames as Swarm load-balances.</p>
       </body>
